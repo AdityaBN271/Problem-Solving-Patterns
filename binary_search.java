0d0d2e15b63b7598287c46eae2d3d2160a8cacf3
AdityaBN271 <adityabn2004@gmail.com>
2025-11-15 10:58:53 +0530

    binary search

--- binary_search.java
+++ binary_search.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class binary_search {
     public static int binarySearch(int[] arr, int target) {
+       
         int left = 0;
         int right = arr.length - 1;
 
@@ -20,6 +21,16 @@ public class binary_search {
 
         }
         return -1;
+
+    public static void main(String[] args) {
+        int [] SortedArray = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int target = 14;
+        int result = binarySearch(SortedArray, target);
+        if (result == -1) {
+            System.out.println("Element not found in the array.");
+        } else {
+            System.out.println("Element found at index: " + result);
+    }
 
 
     }
